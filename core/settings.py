313cdf607350200e337f8a619a8aee27d3792336
IbadMukrom <ibadmukrom569@gmail.com>
2021-02-18 23:03:55 +0800

    fix staticfiles

--- core/settings.py
+++ core/settings.py
@@ -126,3 +126,4 @@ STATIC_URL = '/static/'
 # STATIC_ROOT = 'staticfiles'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
